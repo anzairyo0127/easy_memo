c7edc77ad95ac5d2770d086973db77541088f979
anzai.ryo0127 <36684885+anzairyo0127@users.noreply.github.com>
2021-08-19 20:18:41 +0900

    バイナリを読み込まないように

--- src/menu/file.ts
+++ src/menu/file.ts
@@ -4,6 +4,7 @@ import crypto from "crypto";
 import { ipcMain, dialog } from "electron";
 import chardet from "chardet";
 import iconv from "iconv-lite";
+import { isBinary, isText } from "istextorbinary";
 
 import { FILE_EVENTS } from "../interfaces";
 import { mainWindow, fileStore, title } from "../main";
@@ -15,24 +16,29 @@ const setFilePath = (filePath: string) => {
 
 export const openFileFromMenu = async () => {
   const result = await dialog.showOpenDialog(mainWindow, {
-    properties: ["openFile"],
-    filters: [{name: "all", extensions: ["*"]}]
+    properties: ["openFile", "showHiddenFiles"],
+    filters: [
+      {name: "text", extensions: ["txt", "rtf", "text", "json", "yml", "yaml", "md", "htm", "html", "css", "scss", "sass", "js", "ts", "vue", "tsx", "csv", "xml", "asp", "rs", "py", "c", "cpp", "coffee", "cs", "go", "java", "xhtml", "jsx", "lua", "sql", "pl", "php", "scala", "vbs"]},
+      {name: "all", extensions: ["*"]}
+    ]
   });
   if (result.canceled) return;
   const filePath = result.filePaths[0];
   try {
+    if (!isText(filePath)) throw Error(`${filePath} is not text file`);
     const fileTextBuffer = fs.readFileSync(filePath);
-    const encodeType =  chardet.detect(fileTextBuffer);
-    const fileText = iconv.decode(fileTextBuffer, encodeType as string);
-    fileStore.set("encodeType", encodeType as string);
+    const encodeType = chardet.detect(fileTextBuffer) as string;
+    const fileText = iconv.decode(fileTextBuffer, encodeType);
+    fileStore.set("encodeType", encodeType);
     fileStore.set("fileText", fileText);
     fileStore.set("fileTextAsHash", crypto.createHash("sha256").update(fileText).digest("hex"));
     setFilePath(filePath);
     mainWindow.webContents.send(FILE_EVENTS.OPEN_DIALOG, {fileText, encodeType});
   } catch (e) {
     console.log(e);
-    dialog.showErrorBox("ファイル読み込みエラー", `ファイルの読み込みに失敗しました。対応していないファイルかもしれません。
--------debug_message-------
+    dialog.showErrorBox("ファイル読み込みエラー", 
+`ファイルの読み込みに失敗しました。
+-------message-------
 ${e}`)
   };
 };
@@ -47,7 +53,12 @@ ipcMain.on(FILE_EVENTS.TEXT_CHANGE, (_, fileText) => {
 
 ipcMain.on(FILE_EVENTS.SAVE_FILE, async (_, isSaveAs:boolean) => {
   const fileText = fileStore.get("fileText");
-  const fileEncodeType = fileStore.get("encodeType");
+  let fileEncodeType = fileStore.get("encodeType");
+  if (!fileEncodeType) {
+    fileStore.set("encodeType", "UTF-8");
+    fileEncodeType = "UTF-8";
+    mainWindow.webContents.send(FILE_EVENTS.OPEN_DIALOG, {fileText, encodeType: fileEncodeType});
+  }
   let filePath = fileStore.get("filePath");
   if (isSaveAs || !filePath) {
     const result = await dialog.showSaveDialog(mainWindow, {
@@ -68,7 +79,7 @@ ipcMain.on(FILE_EVENTS.SAVE_FILE, async (_, isSaveAs:boolean) => {
     console.log(e);
     dialog.showErrorBox("ファイル書き込みエラー", 
     `ファイルの書き込みに失敗しました。
--------debug_message-------
+-------message-------
 ${e}`)
   };
 });
